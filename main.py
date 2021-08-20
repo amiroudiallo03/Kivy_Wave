3f6e4daac0dc10f22241bce7f2506a62b09f7eb6
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-08-20 15:24:05 +0000

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from kivymd.app import MDApp
+from kivy.properties import ObjectProperty
+from navigation_screen_manager import NavigationScreenManager
 
 
+class MyScreenManager(NavigationScreenManager):
+    pass
 
 class MainApp(MDApp):
-    pass
+    manager = ObjectProperty(None)
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
+        
 
 
 MainApp().run()
